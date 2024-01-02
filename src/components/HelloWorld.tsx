560fee4ed2ae479d98f4ad06216debb9437b34ce
Smit Desai <smit.desai16@gmail.com>
2024-01-02 10:59:19 -0600

    Sending name from input box

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,8 +1,10 @@
 import { Button, Input, useId } from "@fluentui/react-components";
 import { useTranslation } from "react-i18next";
 import { castSession } from "../chromecastSessionProvider";
+import { useState } from "react";
 
 const HelloWorld = () => {
+    const [text, setText] = useState("");
     const { t } = useTranslation();
     const inputTextId = useId("inputTextId");
 
@@ -10,15 +12,21 @@ const HelloWorld = () => {
         if (castSession) {
             castSession.sendMessage('urn:x-cast:com.example.castdata', {
                 type: "message",
-                text: "Smit"
+                text: text
             });
+            setText("");
         }
     };
+
+    const onTextChange = (event: { target: { value: string; }; }) => {
+        const value = event.target.value;
+        setText(value);
+    }
 
     return (
         <>
             <div style={{ display: "flex" }}>
-                <Input appearance="underline" id={inputTextId} style={{ flexGrow: 1 }} />
+                <Input appearance="underline" id={inputTextId} style={{ flexGrow: 1 }} value={text} onChange={onTextChange} />
                 <Button appearance="primary" onClick={onSendClick}>{t("HelloWorld.Send")}</Button>
             </div>
         </>
